65c3caaeddea7944d7a2540d8f6857500781e2f8
Muhammad Aarish <223570@students.au.edu.pk>
2025-09-27 01:15:38 +0500

    Resolved CORS Issues

--- src/app.js
+++ src/app.js
@@ -23,6 +23,20 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+    const allowedOrigins = ["https://chatapp-client-iota.vercel.app"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.use("/api/v1/users", userRouter);
 
